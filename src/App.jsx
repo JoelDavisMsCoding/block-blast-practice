6e16dc09d20e2979a106764a69b40f665b2e1ff1
JoelDavisMsCoding <166549396+JoelDavisMsCoding@users.noreply.github.com>
2025-12-16 20:16:51 -0600

    Ghost view & DragDrop
    just got these working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import GameBoard from "./components/GameBoard";
 import { generateThreePieces } from "./utils/pieces";
+
 
 function createEmptyBoard() {
   return Array(8).fill().map(() => Array(8).fill(0)); //The two arrays functions make it 8 X 8. first 8 =row /second 8 = columns
@@ -10,13 +11,64 @@ function App() {
   const [board, setBoard] = useState(createEmptyBoard());
   const [availablePieces, setAvailablePieces] = useState(generateThreePieces());
   const [draggedPieceIndex, setDraggedPieceIndex] = useState(null);
+  const [hoverCell, setHoverCell] = useState(null);
 
-  function handleCellClick(row, col){
-    setBoard(prevBoard => {
-      const newBoard = prevBoard.map(arr => [...arr]); //Makes a copy of board(DO NOT ALTER ORIGIONAL board!!)
-      newBoard[row][col] = newBoard[row][col] ? 0 : 1; //Uses copied board to make new changes to current cell.
-      return newBoard;
+  function handleDragOver(e, row, col){
+    e.preventDefault();
+    setHoverCell({row, col})
+  }
+
+  function handleDrop(e, row, col){
+    e.preventDefault();
+
+    const piece = availablePieces[draggedPieceIndex];
+    if (!piece){return;} //If no piece return/do nothing
+
+    if (!canPlacePiece(piece, row, col)){ //If you can not place the piece...
+      console.log("Invalid Placement");
+      return;
+    }
+
+    //Place the piece on the board
+    setBoard(prev => {  //prev is a argument made by me but the first argument will always be set as a copy of previous state/board
+      const newboard = prev.map(r => [...r]); //Saving a copy of the board as newBoard
+      piece.block.forEach((rArr, rIndex) =>{ //piece comes from argument/block comes from pieces.js
+        rArr.forEach((val, cIndex) =>{
+          if (val === 1){
+            newboard[row + rIndex][col + cIndex] = 1;
+          }
+        });
+      });
+      return newboard
     });
+
+    //Remove used piece
+    const updatedPieces = [...availablePieces];
+    updatedPieces[draggedPieceIndex] = null;
+    setAvailablePieces(updatedPieces);
+
+    setHoverCell(null); //Clears/Stops the highlight after dropping the piece/releasing the mouse.
+    setDraggedPieceIndex(null);
+  }
+  
+  function canPlacePiece(piece, startRow, startCol){
+    for (let r = 0; r < piece.block.length; r++) {
+      for (let c = 0; c < piece.block[r].length; c++){
+        if (piece.block[r][c] === 1){
+          let boardR = startRow + r;
+          let boardC = startCol + c;
+
+          if (boardR < 0 || boardR >= 8 || boardC < 0 || boardC >= 8){ //Check to make sure piece is in bound of the board by Row/Col
+            return false;
+          }
+
+          if (board[boardR][boardC] === 1){ //making sure piece is not piece is not placed over existing piece on the board.
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
   return (
@@ -24,7 +76,11 @@ function App() {
       <h1>🧱 Block Blast Practice</h1>
       <GameBoard
         board={board}
-        onCellClick={handleCellClick}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+        hoverCell={hoverCell}
+        canPlacePiece={canPlacePiece}
+        draggedPiece={draggedPieceIndex !== null ? availablePieces[draggedPieceIndex] : null} //if piece is selected pass in piece else pass null
       /> 
       <div className="pieces-area">
         {availablePieces.map((piece, index) =>
@@ -32,17 +88,19 @@ function App() {
             <div
               key={index}
               className="piece"
-              style={{gridTemplateColumns: `repeat(${piece.block[0].length}, 50px)`}}
+              style={{gridTemplateColumns: `repeat(${piece.block[0].length}, 50px)`}} //repeat() Make same # of col as rows
               draggable
               onDragStart={() => setDraggedPieceIndex(index)}
             >
               {piece.block.map((row, r) =>
-                row.map((val, c) => (
+                row.map((val, c) =>
                   <div
                     key={`${r}-${c}`}
                     className={`mini-cell ${val ? "filled" : ""}`}
-                  />
-                ))
+                  >
+                    {c}{r}
+                  </div>
+                )
               )}
             </div>
           ) : (
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,39 @@
 import React from "react";
 
-function GameBoard({board, onCellClick,}) {
+function GameBoard({board, hoverCell, draggedPiece, canPlacePiece, onDragOver, onDrop}) {
   return (
     <div className="board">
       {board.map((row, rIndex) => 
-        row.map((cell, cIndex) => ( //cell will have the rows and columns 8 by 8 when you print/display cell.
+        row.map((cell, cIndex) => { //cell will have the rows and columns 8 by 8 when you print/display cell.
+          let ghostClass = ""
 
-          <div
-            key={`${rIndex}-${cIndex}`}
-            className={`cell ${cell ? "filled" : ""}`} //Adds the class name filled or leaves it cell(empty)
-            onClick={() => onCellClick(rIndex, cIndex)}
-          />
-        )) //End row.map parenthesis
+          if (hoverCell && draggedPiece){
+            const startR = hoverCell.row;
+            const startC = hoverCell.col;
+            
+            const canPlace = canPlacePiece(draggedPiece, startR, startC);
+
+            for (let r = 0; r < draggedPiece.block.length; r++){
+              for (let c = 0; c < draggedPiece.block[r].length; c++){
+                if (draggedPiece.block[r][c] === 1){
+                  if (startR + r === rIndex && startC + c === cIndex){
+                    ghostClass = canPlace ? "ghost-valid" : "ghost-invalid";
+                  }
+                }
+              }
+            }
+          }
+          return (
+            <div
+              key={`${rIndex}-${cIndex}`}
+              className={`cell ${cell ? "filled" : ""} ${ghostClass}`}
+              onDragOver={(e) => onDragOver(e, rIndex, cIndex)}
+              onDrop={(e) => onDrop(e,rIndex, cIndex)}
+            >
+              {rIndex}{cIndex}
+            </div>
+          );
+        }) //End row.map parenthesis
       )}
     </div> //End className board div
   );
